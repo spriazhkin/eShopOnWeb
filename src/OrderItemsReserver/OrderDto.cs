b60478fef52c9d64910e31152a8ad6ff818947ce
Sergei Priazhkin <spryazhkin@gmail.com>
2021-07-14 15:55:35 +0300

    Added reserver function

---
+++ src/OrderItemsReserver/OrderDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderItemsReserver
+{
+    internal class OrderDto
+    {
+        [Required]
+        public string ItemId { get; set; }
+        
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Only positive count allowed")]
+        public int? Count { get; set; }
+    }
+}
