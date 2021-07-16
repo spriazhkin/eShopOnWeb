cbecccf919977dde8a8fc1860d2a7a0e79b7e6cb
Sergei Priazhkin <s.pryazhkin@gmail.com>
2021-07-16 12:19:44 +0300

    Code to call function

---
+++ src/Infrastructure/Services/OrderItemDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Microsoft.eShopWeb.Infrastructure.Services
+{
+    internal class OrderItemDto
+    {
+        public string ItemId { get; set; }
+
+        public int? Count { get; set; }
+    }
+}
